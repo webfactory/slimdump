ef53c13a9a4a9947ecff859f90732b1a9865ac03
Matthias Molitor <mmo@webfactory.de>
2014-11-21 13:32:29 +0100

    reformatted code

--- php/util.php
+++ php/util.php
@@ -1,54 +1,69 @@
 <?php
 
-    /**
-     * @param string $dsn
-     * @return Zend_Db_Adapter_Abstract
-     */
-	function connect($dsn) {
-		try {
-			$db = Zend_Db::factory('Pdo_Mysql', parseDSN($dsn));
-			$db->query('SET NAMES utf8');
-			return $db;
-		} catch (Exception $e) {
-			fail("Database error: " . $e->getMessage());
-		}
-	}
+/**
+ * @param string $dsn
+ * @return Zend_Db_Adapter_Abstract
+ */
+function connect($dsn)
+{
+    try {
+        $db = Zend_Db::factory('Pdo_Mysql', parseDSN($dsn));
+        $db->query('SET NAMES utf8');
+        return $db;
+    } catch (Exception $e) {
+        fail("Database error: " . $e->getMessage());
+    }
+}
 
-	function parseDSN($dsn) {
-		if(!preg_match('~^mysql://([^:@]*)(?::([^@]+))?@([^:/]+)(?::(\d+))?/([\w-]+)$~', $dsn, $matches)) throw new Exception('DSN could not be parsed');
-
-		$params = array();
-
-		if(isset($matches[1])) $params['username'] = $matches[1];
-		if(isset($matches[2])) $params['password'] = $matches[2];
-		if(isset($matches[3])) $params['host'] = $matches[3];
-		if(isset($matches[4])) $params['port'] = $matches[4];
-		if(isset($matches[5])) $params['dbname'] = $matches[5];
-
-		return  $params;
-	}
-
-	function fail($msg) {
-		fwrite(STDERR, "$msg\n");
-		exit(1);
-	}
-	
-	function warn($msg) {
-		fwrite(STDERR, "Warn: $msg\n");
-	}
-
-    function rglob($pattern, $flags = 0, $path = '') {
-        if (!$path && ($dir = dirname($pattern)) != '.') {
-            if ($dir == '\\' || $dir == '/') {
-                $dir = '';
-            }
-            return rglob(basename($pattern), $flags, $dir . '/');
-        }
-        $paths = glob($path . '*', GLOB_ONLYDIR | GLOB_NOSORT);
-        $files = glob($path . $pattern, $flags);
-        foreach ($paths as $p) {
-            $files = array_merge($files, rglob($pattern, $flags, $p . '/'));
-        }
-        return $files;
+function parseDSN($dsn)
+{
+    if (!preg_match('~^mysql://([^:@]*)(?::([^@]+))?@([^:/]+)(?::(\d+))?/([\w-]+)$~', $dsn, $matches)) {
+        throw new Exception('DSN could not be parsed');
     }
 
+    $params = array();
+    if (isset($matches[1])) {
+        $params['username'] = $matches[1];
+    }
+    if (isset($matches[2])) {
+        $params['password'] = $matches[2];
+    }
+    if (isset($matches[3])) {
+        $params['host'] = $matches[3];
+    }
+    if (isset($matches[4])) {
+        $params['port'] = $matches[4];
+    }
+    if (isset($matches[5])) {
+        $params['dbname'] = $matches[5];
+    }
+
+    return $params;
+}
+
+function fail($msg)
+{
+    fwrite(STDERR, "$msg\n");
+    exit(1);
+}
+
+function warn($msg)
+{
+    fwrite(STDERR, "Warn: $msg\n");
+}
+
+function rglob($pattern, $flags = 0, $path = '')
+{
+    if (!$path && ($dir = dirname($pattern)) != '.') {
+        if ($dir == '\\' || $dir == '/') {
+            $dir = '';
+        }
+        return rglob(basename($pattern), $flags, $dir . '/');
+    }
+    $paths = glob($path . '*', GLOB_ONLYDIR | GLOB_NOSORT);
+    $files = glob($path . $pattern, $flags);
+    foreach ($paths as $p) {
+        $files = array_merge($files, rglob($pattern, $flags, $p . '/'));
+    }
+    return $files;
+}
