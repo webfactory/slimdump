9e30f2c83c6de43577444d79f938e081ced26d39
Dominik Ritter <dritter03@googlemail.com>
2015-10-16 12:08:05 +0200

    Extracted class `Slimdump` from Slimdump.php

--- bin/slimdump.php
+++ bin/slimdump.php
@@ -1,37 +1,10 @@
 <?php
 
-use \Webfactory\Slimdump\Config\Config;
 use \Webfactory\Slimdump\Config\ConfigBuilder;
-use \Webfactory\Slimdump\Database\Dumper;
+use \Webfactory\Slimdump\Slimdump;
 
 $db = connect(array_shift($_SERVER['argv']));
 
 $config = ConfigBuilder::createConfigurationFromConsecutiveFiles($_SERVER['argv']);
-
-processConfig($config, $db);
-
-/**
- * @param Config $config
- * @param Zend_Db_Adapter_Abstract $db
- * @internal param string $file
- */
-function processConfig(Config $config, $db)
-{
-    $dumper = new Dumper();
-
-    foreach ($db->listTables() as $tableName) {
-        $tableConfig = $config->findTable($tableName);
-
-        if (null === $tableConfig) {
-            continue;
-        }
-
-        if ($tableConfig->isSchemaDumpRequired()) {
-            $dumper->dumpSchema($tableName, $db);
-
-            if ($tableConfig->isDataDumpRequired()) {
-                $dumper->dumpData($tableName, $tableConfig, $db);
-            }
-        }
-    }
-}
+$slimdump = new Slimdump();
+$slimdump->dump($config, $db);
---
+++ src/Webfactory/Slimdump/Slimdump.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Webfactory\Slimdump;
+
+use Webfactory\Slimdump\Config\Config;
+use Webfactory\Slimdump\Database\Dumper;
+
+class Slimdump
+{
+
+    /**
+     * @param Config $config
+     * @param \Zend_Db_Adapter_Abstract $db
+     */
+    public function dump(Config $config, $db)
+    {
+        $dumper = new Dumper();
+
+        foreach ($db->listTables() as $tableName) {
+            $tableConfig = $config->findTable($tableName);
+
+            if (null === $tableConfig) {
+                continue;
+            }
+
+            if ($tableConfig->isSchemaDumpRequired()) {
+                $dumper->dumpSchema($tableName, $db);
+
+                if ($tableConfig->isDataDumpRequired()) {
+                    $dumper->dumpData($tableName, $tableConfig, $db);
+                }
+            }
+        }
+    }
+
+}
