0cd2369a09bbd371a6119d8b426956c7d6458111
franziskahahn <franziskavictoriahahn@yahoo.de>
2017-01-20 13:57:32 +0100

    add-faker-integration: Added missing Exception

---
+++ src/Webfactory/Slimdump/Exception/InvalidReplacementOptionException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Webfactory\Slimdump\Exception;
+
+/**
+ * Exception for invalid values of "replacement" option
+ * @package Webfactory\Slimdump\Exception
+ */
+class InvalidReplacementOptionException extends \Exception
+{
+
+}
