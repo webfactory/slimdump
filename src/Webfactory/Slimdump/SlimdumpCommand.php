webfactory/slimdump

commit e6c61184b120045a7b56ab0baea8c07a82099d40

Use Regex to assert position at start of line of 'mysqli' inside dsn parameter

src/Webfactory/Slimdump/SlimdumpCommand.php
@@ -62,7 +62,7 @@ class SlimdumpCommand extends Command
              * a dsn string containing mysqli will result in the DriverManager creating a MysqliConnection but we need PDO
              * @see DriverManager::parseDatabaseUrlScheme()
              */
-            $mysqliIndependentDsn = str_replace('mysqli://', 'mysql://', $dsn);
+            $mysqliIndependentDsn = preg_replace('_^mysqli://_', 'mysql:', $dsn);
 
             return DriverManager::getConnection(
                 array('url' => $mysqliIndependentDsn, 'charset' => 'utf8', 'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver')

commit 141d02bca66c648aa1003face6d0a28ddac29835

Fix glitch handling the mysqli:// scheme
Introduced in e6c6118.

src/Webfactory/Slimdump/SlimdumpCommand.php
@@ -62,7 +62,7 @@ class SlimdumpCommand extends Command
              * a dsn string containing mysqli will result in the DriverManager creating a MysqliConnection but we need PDO
              * @see DriverManager::parseDatabaseUrlScheme()
              */
-            $mysqliIndependentDsn = preg_replace('_^mysqli://_', 'mysql:', $dsn);
+            $mysqliIndependentDsn = preg_replace('_^mysqli:_', 'mysql:', $dsn);
 
             return DriverManager::getConnection(
                 array('url' => $mysqliIndependentDsn, 'charset' => 'utf8', 'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver')