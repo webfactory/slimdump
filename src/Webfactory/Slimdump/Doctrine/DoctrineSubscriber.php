7864e955d087d5bf2bdb5319c5ed52bcda23794a
Matthias Pigulla <mp@webfactory.de>
2024-01-18 10:26:39 +0100

    Fill in dummy types to deal with DC2:... column comments

---
+++ src/Webfactory/Slimdump/Doctrine/DoctrineSubscriber.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Webfactory\Slimdump\Doctrine;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\DBAL\Event\SchemaColumnDefinitionEventArgs;
+use Doctrine\DBAL\Events;
+use Doctrine\DBAL\Schema\AbstractSchemaManager;
+use Doctrine\DBAL\Types\StringType;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\TypeRegistry;
+use Doctrine\DBAL\Types\Types;
+
+class DoctrineSubscriber implements EventSubscriber
+{
+    /**
+     * @var AbstractSchemaManager
+     */
+    private $schemaManager;
+
+    public function __construct(AbstractSchemaManager $schemaManager)
+    {
+        $this->schemaManager = $schemaManager;
+    }
+
+    public function getSubscribedEvents(): array
+    {
+        return [Events::onSchemaColumnDefinition];
+    }
+
+    public function onSchemaColumnDefinition(SchemaColumnDefinitionEventArgs $event): void
+    {
+        $tableColumn = array_change_key_case($event->getTableColumn(), CASE_LOWER);
+        $dbType = strtolower($tableColumn['type']);
+        $dbType = strtok($dbType, '(), ');
+
+        if (isset($tableColumn['comment'])) {
+            $type = $this->schemaManager->extractDoctrineTypeFromComment($tableColumn['comment'], '');
+
+
+            if ($type && !Type::hasType($type)) {
+                Type::addType($type, DummyType::class);
+            }
+        }
+
+        $databasePlatform = $this->schemaManager->getDatabasePlatform();
+        if (! $databasePlatform->hasDoctrineTypeMappingFor($dbType)) {
+            if (!Type::hasType(DummyType::NAME)) {
+                Type::addType(DummyType::NAME, DummyType::class);
+            }
+            $databasePlatform->registerDoctrineTypeMapping($dbType, DummyType::NAME);
+        }
+    }
+}
--- src/Webfactory/Slimdump/SlimdumpCommand.php
+++ src/Webfactory/Slimdump/SlimdumpCommand.php
@@ -18,6 +18,7 @@ use Webfactory\Slimdump\Config\ConfigBuilder;
 use Webfactory\Slimdump\Database\CsvOutputFormatDriver;
 use Webfactory\Slimdump\Database\MysqlOutputFormatDriver;
 use Webfactory\Slimdump\Database\OutputFormatDriverInterface;
+use Webfactory\Slimdump\Doctrine\DoctrineSubscriber;
 
 final class SlimdumpCommand extends Command
 {
@@ -74,7 +75,7 @@ final class SlimdumpCommand extends Command
         $progressOutput = $this->createProgressOutput($input, $output);
 
         $connection = $this->createConnection($input);
-        $connection->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+        $connection->getEventManager()->addEventSubscriber(new DoctrineSubscriber($connection->getSchemaManager()));
         $this->setMaxExecutionTimeUnlimited($connection, $progressOutput);
 
         $config = ConfigBuilder::createConfigurationFromConsecutiveFiles($input->getArgument('config'));
