e3839224d16a2da52fe85a4becf60bdee6d33572
Matthias Pigulla <mp@webfactory.de>
2024-08-26 19:29:44 +0200

    Nutze Docker-Image von friendsofphp/php-cs-fixer
    Automatische erstellte Änderung über https://github.com/webfactory/bfg9000/pull/9 (Case 174703)

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,8 +1,10 @@
 <?php
 
 return (new PhpCsFixer\Config())
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setRules([
         '@Symfony' => true,
+        'phpdoc_separation' => false,
         '@Symfony:risky' => true,
         'array_syntax' => array('syntax' => 'short'),
         'no_unreachable_default_argument_value' => false,
